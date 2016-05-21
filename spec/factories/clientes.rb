f2bfe08ac907cf7402f487b4fed9fbb2f3b33174
viniciushof <viniciush.oliveira@gmail.com>
2016-05-21 16:29:58 -0300

    rspec

--- spec/factories/clientes.rb
+++ spec/factories/clientes.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :cliente do
-    
+	sequence(:nome) {|n| "Cliente#{n}"}
+	data_nascimento Date.new(2000, 12, 31)    
   end
 end
--- spec/models/cliente_spec.rb
+++ spec/models/cliente_spec.rb
@@ -2,22 +2,29 @@ require 'rails_helper'
 
 RSpec.describe Cliente, type: :model do
   	it "is valid with a nome and data_nascimento" do
-		cliente = Cliente.new(nome: "Felipe", data_nascimento: Date.new(2000, 12, 1))
-		expect(cliente).to be_valid
-		end
+		#cliente = Cliente.new(nome: "Felipe", data_nascimento: Date.new(2000, 12, 1))
+		#expect(cliente).to be_valid
+		expect(FactoryGirl.build(:cliente)).to be_valid
+	end
 	it "is invalid without a nome" do
-		cliente = Cliente.new(nome: nil,data_nascimento: Date.new(2000, 12, 1))
-		expect(cliente).not_to be_valid
-		expect(cliente.errors[:nome]).to include("deve ser preenchido")
+		#cliente = Cliente.new(nome: nil,data_nascimento: Date.new(2000, 12, 1))
+		#expect(cliente).not_to be_valid
+		#expect(cliente.errors[:nome]).to include("deve ser preenchido")
+		cliente = build(:cliente, nome: nil)
+		cliente.valid?
+		expect(cliente.errors[:nome]).to include("Deve ser preenchido")
 	end
 	it "is invalid without a data_nascimento"
 	it "is invalid when data_nascimento is before 31/12/1999"
 	it "is valid when data_nascimento is after 31/12/1999"
 	describe "it can filter nome by letter" do
 		before :each do
-			@felipe = Cliente.create(nome: "Felipe", data_nascimento: Date.new(2000, 12, 31))
-			@joao = Cliente.create(	nome: "João", data_nascimento: Date.new(2005, 12, 10))
-			@fernando = Cliente.create(nome: "Fernando", data_nascimento: Date.new(2013, 2, 1))
+			#@felipe = Cliente.create(nome: "Felipe", data_nascimento: Date.new(2000, 12, 31))
+			@felipe = build(:cliente, nome:"Felipe")
+			#@joao = Cliente.create(	nome: "João", data_nascimento: Date.new(2005, 12, 10))
+			@joao = build(:cliente, nome: "João")
+			#@fernando = Cliente.create(nome: "Fernando", data_nascimento: Date.new(2013, 2, 1))
+			@fernando = build.(:cliente, nome: "Fernando")
 		end
 		context "matching letter" do
 			# Testa se incluiu corretamente
