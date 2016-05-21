d8f595bc94d5bb58cc82a02fb984d84d67711ef8
viniciushof <viniciush.oliveira@gmail.com>
2016-05-21 11:01:52 -0300

    scaffold cliente

---
+++ app/views/clientes/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @cliente, :id, :nome, :data_nascimento, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :clientes
   root to: 'restaurantes#index'
   match 'inicio' => 'restaurantes#index', via: 'get'
   resources :restaurantes
